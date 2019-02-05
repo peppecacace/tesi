b48544ceab823da1d4ae03c2f35322e05988ee06
peppecacace <giuseppe.cacace10@gmail.com>
2019-02-05 17:14:33 +0100

    commit 05/02 da vedere la test accuracy assolutamente

---
+++ main.py
@@ -0,0 +1,140 @@
+import numpy as np
+from processing import pre_processing, training
+from after_training import generation, predict
+import music21
+
+w = 15 #la nostra sequenza
+max_quarter = 8   #massimi quarti di battuta
+epochs = 50
+batch_size = 16
+train = True
+
+def main():
+    allnotes, alldurations, songs = pre_processing.get_notes(max_quarter)
+
+    vocab = sorted(
+    set(item for item in allnotes))  # nel set trovo l'alfabeto di note viste (associate al pitch) - pitch names
+    print('pitch names: ' + str(vocab))
+
+    n_vocab = len(set(allnotes))
+    print('pitch length: ' + str(n_vocab))
+
+    durations_vocab = len(set(alldurations))
+    print('durations length: ' + str(durations_vocab))
+
+    if(train):
+
+        note_input, note_output, duration_input, duration_output = pre_processing.prepare_sequences(allnotes, w, songs)
+
+        model = training.create_network(note_input, n_vocab, duration_input, durations_vocab)
+
+
+    n = note_input.shape[0]  # 56468x32x354
+    print('dimensioni dataset: ' + str(note_input.shape[0]) + ' finestre da ' + str(
+        note_input.shape[1]) + ' con dimensione one-hot di ' + str(note_input.shape[2]))
+
+    dslice = int(n * 1 / 10)
+
+    note_x_train = note_input[:dslice*8]
+    note_x_val = note_input[dslice*8:dslice*9]
+    note_x_test = note_input[dslice*9:]
+
+    time_x_train = duration_input[:dslice*8]
+    time_x_val = duration_input[dslice*8:dslice*9]
+    time_x_test = duration_input[dslice*9:]
+
+    note_y_train = note_output[:dslice*8]
+    note_y_val = note_output[dslice*8:dslice*9]
+    note_y_test = note_output[dslice*9:]
+
+    time_y_train = duration_output[:dslice*8]
+    time_y_val = duration_output[dslice*8:dslice*9]
+    time_y_test = duration_output[dslice*9:]
+
+    t = training.train(model, note_x_train, note_y_train, time_x_train, time_y_train, epochs, batch_size, note_x_val, note_y_val, time_x_val, time_y_val)
+    training.plottraining(t)
+    model.load_weights('mymodel')
+
+    scores2 = model.evaluate([note_x_test, time_x_test], [note_y_test, time_y_test])
+
+
+    print(
+        'valutazione sul test set : output_notes_loss: {} - output_times_loss: {} - output_notes_acc: {} - output_times_acc: {}'.format(scores2[1],
+                                                                                                             scores2[2],
+                                                                                                             scores2[3],
+                                                                                                             scores2[
+                                                                                                                 4]))
+
+    # GENERAZIONE
+    generation_note, generation_time, seed_note, seed_time, original_note, original_time = generation.free_run(model, note_input,
+                                                                                                    duration_input,
+                                                                                                    vocab, n_vocab,
+                                                                                                    durations_vocab)  # generation_time (100, 1, 8) - generation_note (100,)
+    create_midi(generation_note, generation_time,
+                'generation')  # generation note -> note argmaxate - generation_time -> durate non argmaxate
+
+    create_midi(original_note, original_time, 'generationoriginal')
+
+    create_midi(seed_note, seed_time, 'generationseed')
+
+    # PREDIZIONE
+    p_note, p_time = predict.prediction(model, note_x_test, time_x_test, note_y_test, time_y_test, vocab, n_vocab, durations_vocab)
+    create_midi(p_note, p_time, 'prediction')
+
+    # ORIGINALE PREDIZIONE
+    pnotes = predict.original_prediction(vocab, note_y_test)
+    create_midi(pnotes, time_y_test, 'original')
+
+
+def create_midi(note, time, name):
+    offset = 0
+    output_notes = []
+
+    prediction_time = np.asarray(time)  # prediction_time shape (14117, 8)
+
+    if name == 'generation':
+        prediction_time = np.argmax(prediction_time, axis=2).astype('float32')
+    if name == 'prediction':
+        prediction_time = np.argmax(prediction_time, axis=1).astype('float32')
+    if name == 'generationoriginal':
+        prediction_time = np.argmax(prediction_time, axis=2).astype('float32')
+    if name == 'generationseed':
+        prediction_time = np.argmax(prediction_time, axis=2).astype('float32')
+
+    # crea oggetti di note e accordi sulla base dei valori generati dal modello
+    i = 0
+    for pattern in note:
+
+        # pattern è un accordo
+        if ('.' in pattern) or pattern.isdigit():
+            notes_in_chord = pattern.split('.')
+            notes = []
+            for current_note in notes_in_chord:
+                new_note = music21.note.Note(int(current_note))
+                new_note.storedInstrument = music21.instrument.Piano()
+                notes.append(new_note)
+            new_chord = music21.chord.Chord(notes)
+            t = prediction_time[i]/4.
+            new_chord.quarterLength = float(t)
+            new_chord.offset = offset
+            output_notes.append(new_chord)
+        # pattern is a note
+        else:
+            new_note = music21.note.Note(pattern)
+            t = prediction_time[i]/4.
+            new_note.quarterLength = float(t)
+            new_note.offset = offset
+            new_note.storedInstrument = music21.instrument.Piano()
+            output_notes.append(new_note)
+
+        i = i + 1
+
+        # increase offset each iteration so that notes do not stack
+        offset += 0.5
+
+    midi_stream = music21.stream.Stream(output_notes)
+
+    midi_stream.write('midi', fp='results/' + name + '.mid')
+
+if __name__ == '__main__':
+    main()
--- processing/pre_processing.py
+++ processing/pre_processing.py
@@ -3,15 +3,40 @@ import pickle
 import numpy
 from music21 import converter, instrument, note, chord
 from keras.utils import np_utils
+import os
+import random
 
 def get_notes(max_quarter):
     """ Get all the notes and chords from the midi files in the ./midi_songs directory """
     dataset_path = 'midi_songs'
-    notes = []
-    durations = []
+    skip_path = 'processing/data'
+
+    allnotes = []
+    alldurations = []
+    songs = []
+
+    if os.path.isfile(skip_path+'/seen_notes') and os.path.isfile(skip_path+'/seen_durations') and os.path.isfile(skip_path+'/seen_songs'):
+
+        allnotes = None
+        alldurations = None
+        songs = None
+
+        with open(r''+skip_path+'/seen_notes', "rb") as input_file:
+            allnotes = pickle.load(input_file)
+
+        with open(r''+skip_path+'/seen_durations', "rb") as input_file:
+            alldurations = pickle.load(input_file)
+
+        with open(r''+skip_path+'/seen_songs', "rb") as input_file:
+            songs = pickle.load(input_file)
+
+        return allnotes, alldurations, songs
 
     for file in glob.glob(dataset_path + '/*.mid'):
         midi = converter.parse(file)
+
+        notes = []
+        durations = []
 
         print('Parsing {}'.format(file))
 
@@ -28,35 +53,44 @@ def get_notes(max_quarter):
 
                 if s > 0:
                     durations.append(s if s < max_quarter else max_quarter - 1)
+                    alldurations.append(s if s < max_quarter else max_quarter - 1)
 
                     n = str(element.pitch)
                     notes.append(n)
+                    allnotes.append(n)
             elif isinstance(element, chord.Chord):  # se è un accordo
                 s = int(4 * float(element.duration.quarterLength))
 
                 if s > 0:
                     durations.append(s if s < max_quarter else max_quarter - 1)
+                    alldurations.append(s if s < max_quarter else max_quarter - 1)
 
                     n = '.'.join(str(n) for n in element.normalOrder)
                     notes.append(n)
+                    allnotes.append(n)
 
-    durations = numpy.asarray(durations).astype('int32')
+        songs.append([notes, durations])
 
-    with open("data" + '/seen_notes', 'wb') as filepath:
-        pickle.dump(notes, filepath)
+    random.shuffle(songs)
 
-    with open("data" + '/seen_durations', 'wb') as filepath:
-        pickle.dump(durations, filepath)
+    with open("processing/data" + '/seen_notes', 'wb') as filepath:
+        pickle.dump(allnotes, filepath)
 
-    return notes, durations
+    with open("processing/data" + '/seen_durations', 'wb') as filepath:
+        pickle.dump(alldurations, filepath)
 
-def prepare_sequences(notes, n_vocab, durations, w):
+    with open("processing/data" + '/seen_songs', 'wb') as filepath:
+        pickle.dump(songs, filepath)
+
+    return allnotes, alldurations, songs
+
+def prepare_sequences(allnotes, w, songs):
     """ Prepare the sequences used by the Neural Network """
 
-    # get all pitch names
-    pitchnames = sorted(set(item for item in notes))
+        # get all pitch names
+    pitchnames = sorted(set(item for item in allnotes))
 
-     # create a dictionary to map pitches to integers
+        # create a dictionary to map pitches to integers
     note_to_int = dict((note, number) for number, note in enumerate(pitchnames))
 
     note_input = []
@@ -66,36 +100,31 @@ def prepare_sequences(notes, n_vocab, durations, w):
     duration_output = []
 
     # create input sequences and the corresponding outputs
-    for i in range(0, len(notes) - w, 1):     ##for (inizio,fine,passo)
-        sequence_in = notes[i:i + w]
-        sequence_out = notes[i + w]
-        note_input.append([note_to_int[char] for char in sequence_in])
-        note_output.append(note_to_int[sequence_out])
 
-        duration_input.append(durations[i:i + w])
-        duration_output.append(durations[i + w])
+    for song in songs:
+        notes = song[0]
+        durations = song[1]
+
+        for i in range(0, len(notes) - w, 1):     ##for (inizio,fine,passo)
+            sequence_in = []
+            for j in range(w):
+                sequence_in.append(note_to_int[notes[i+j]])
+            note_input.append(sequence_in)
+            note_output.append(note_to_int[notes[i + w]])
+
+            duration_input.append(durations[i:i + w])
+            duration_output.append(durations[i + w])
 
     n_patterns = len(note_input)
+
 
     note_output = np_utils.to_categorical(note_output)
     note_input = np_utils.to_categorical(note_input)
 
-    # reshape the input into a format compatible with LSTM layers
-    # note_input = numpy.reshape(note_input, (n_patterns, w, 1))
-    # normalize input
-
-    #note_input = note_input / float(n_vocab)
-
     duration_output = np_utils.to_categorical(duration_output)
+    duration_output = numpy.delete(duration_output, 0, axis=1)
+
     duration_input = np_utils.to_categorical(duration_input)
+    duration_input = numpy.delete(duration_input, 0 , axis=2)
 
     return note_input, note_output, duration_input, duration_output
-
-if __name__ == '__main__':
-    max_quarter = 8  # numero massimo di quarti di battuta
-    w = 15  # finestra
-   # notes, durations = get_notes(max_quarter)
-
- #    n_vocab = len(set(notes))
-
- #   note_input, note_output, duration_input, duration_output = prepare_sequences(notes, n_vocab, durations, w)
